2155c309fc22ebdc9d5bc42816068790a9abd893
Sara-Alcaras <saraalcaras401@gmail.com>
2020-03-10 14:46:26 -0300

    Fix  bug
    indentation was made  and removed the tie bug

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -8,22 +8,27 @@ function JogoDaVelha() {
   const [winner, setWinner] = useState(null);
 
   const handleCellClick = (index) => {
+
     if (winner) {
-    console.log("Game finalized");
-    return null;
-  }
-    if (board[index] !== "") {
-    console.log("Busy Position");
-    return null;
+      console.log("Game finalized");
+      return null;
     }
+
+    if (board[index] !== "") {
+      console.log("Busy Position");
+      return null;
+    }
+
     setBoard(
       board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item)
-      );
+    );
 
-      setCurrentPlayer(currentPlayer === "X" ? "O" : "X" );
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
+
   const checkWinner = () => {
-    const possibleWaysToWin = [ 
+
+    const possibleWaysToWin = [
       [board[0], board[1], board[2]],
       [board[3], board[4], board[5]],
       [board[6], board[7], board[8]],
@@ -33,27 +38,28 @@ function JogoDaVelha() {
       [board[2], board[5], board[8]],
 
       [board[0], board[4], board[8]],
-      [board[2], board[4], board[6]],  
-      [board[3], board[5], board[7]], 
+      [board[2], board[4], board[6]],
+      [board[3], board[5], board[7]],
+      [board[7], board[8], board[9]]
     ];
 
     possibleWaysToWin.forEach(cells => {
       if (cells.every(cell => cell === "O")) setWinner("O");
 
-      if (cells.every(cell => cell === "X")) setWinner("X"); 
+      if (cells.every(cell => cell === "X")) setWinner("X");
     });
-     
+
     checkDraw();
   }
 
   const checkDraw = () => {
     if (board.every(item => item !== "")) setWinner("E");
-      // Empate se todos forem diferentes de vazio
+    // Empate se todos forem diferentes de vazio
   }
 
 
   useEffect(checkWinner, [board]); // So ira passar uma função quando algo for alterado
-  
+
   const resetGame = () => {
     setCurrentPlayer("O");
     setBoard(emptyBoard);
@@ -61,37 +67,38 @@ function JogoDaVelha() {
   }
 
   return (
-    <main>  
+    <main>
       <h1 className="title"> Jogo da Velha</h1>
-    
-      <div className={`board ${winner ? "game-over" : ""}` }>
+
+      <div className={`board ${winner ? "game-over" : ""}`}>
         {board.map((item, index) => (
-          <div 
-           key={index}
-           className={`cell ${item}`}
-           onClick={() => handleCellClick((index))}
+          <div
+            key={index}
+            className={`cell ${item}`}
+            onClick={() => handleCellClick((index))}
           >
             {item}
           </div>
-          ))}
-        </div>
-       {winner &&
-          <footer>
-            {winner === "E" ? 
+        ))}
+      </div>
+      {winner &&
+        <footer>
+          {winner === "E" ?
             <h2 className="winner-message">
-              <span className={winner}>Empatou!</span> 
+              <span className={winner}>Empatou!</span>
             </h2>
             :
             <h2 className="winner-message">
               <span className={winner}>{winner}</span> venceu!
             </h2>
-          } 
+          }
 
-            <button onClick={resetGame}>Recomeçar jogo!</button>
-          </footer> 
-        } 
+          <button onClick={resetGame}>Recomeçar jogo!</button>
+        </footer>
+      }
     </main>
   );
 }
 
 export default JogoDaVelha;
+
