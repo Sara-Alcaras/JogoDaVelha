88cfae1a7f982f29afebadf424f41ca765b5bf4e
Sara-Alcaras <saraalcaras401@gmail.com>
2020-04-02 12:30:35 -0300

    bug fix
    I removed the selection and magnification error

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -94,12 +94,12 @@ function JogoDaVelha() {
 
         </div>
         <h1 className="title"> Jogo da Velha</h1>
-        <h1 className="title2"> Jogo da Velha</h1>
-        
-
+    
         <div className="nextPlayer">
         {`Next Player: ${currentPlayer}`}
         </div>
+
+        <h1 className="title-mobile"> Jogo da Velha</h1>
 
       </div>
     
